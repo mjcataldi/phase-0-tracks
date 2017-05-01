14d1bd05bc907fef1c4f3453a32bab3daa6916e1
Matthew Cataldi <mcataloe@gmail.com>
2017-05-01 16:40:56 +0000

    Initial commit for iteration drill.rb

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -7,19 +7,78 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 
+def asterisk_supplies(supplies)
+  supplies.each { |value| puts "#{value}*" }
+end
+# asterisk_supplies(zombie_apocalypse_supplies)
+
+
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+
+def sort_supplies(supplies)
+  moved = true
+  while moved == true
+  moved = false
+  
+    counter = 0
+    while counter < supplies.length
+      if counter == 0
+        counter += 1
+        next
+      elsif supplies[counter].downcase < supplies[counter - 1].downcase
+        supplies[counter], supplies[counter - 1] = supplies[counter - 1], supplies[counter]
+        moved = true
+      end
+      counter += 1
+    end
+  end
+  supplies
+end
+# sort_supplies(zombie_apocalypse_supplies).each { |value| puts "#{value}." }
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
 
+def supply_in_list(item, supplies)
+  item.strip.downcase!
+  counter = 0
+  reply = ""
+  
+  while counter < supplies.length
+    if supplies[counter].downcase.strip == item
+      reply = "Yes, the supply #{item} is in the list."    
+      break
+    else
+      reply = "No, the supply #{item} is not in the list."    
+    end
+  counter += 1
+  end
+  reply
+end
+# puts supply_in_list("hatchet", zombie_apocalypse_supplies)
+
+
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
+
+def remove_items(supplies)
+  counter = 0
+  
+  while counter < supplies.length
+    if supplies.length > 5
+      supplies.delete_at(counter)
+    end
+  counter += 1
+  end
+  supplies
+end
+# remove_items(zombie_apocalypse_supplies).each { |value| puts "#{value}." }
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -29,6 +88,13 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
+
+# def combine_supplies(supplies1, supplies2)
+#   new_supplies = supplies1 << supplies2
+#   new_supplies.flatten!
+#   new_supplies.uniq!
+# end
+# combine_supplies(zombie_apocalypse_supplies, other_survivor_supplies)
 
 # Hash Drills
 
@@ -46,15 +112,41 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
+def list_endangered_animals(hashed_list)
+  hashed_list.each { |key, value| puts "#{key} - #{value}.*" }
+end
+# list_endangered_animals(extinct_animals)
+
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+
+def before_2000(hashed_list)
+  new_hashed_list = {}
+  
+  hashed_list.each do |key, value|
+    if value >= 2000
+      new_hashed_list << [key, value]
+    end
+  new_hashed_list
+end
+# before_2000(extinct_animals).each { |key, value| puts "#{key} - #{value}" }
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
+def update_list(hashed_list)
+  hashed_list.each {|key, value|
+    hashed_list[value] = value - 3
+  }
+  hashed_list
+end
+puts update_list(extinct_animals).each { |key, value| puts "#{key} - #{value}." }
+
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -63,6 +155,13 @@ extinct_animals = {
 # "Saiga Antelope"
 # Do not use any special built-in methods.
 # ----
+
+def check_if_endagered(check_animal, hashed_list)
+  counter = 0
+  
+  while counter < hashed_list
+
+end
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
