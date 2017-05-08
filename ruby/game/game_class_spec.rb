dd6a5289e26e8f55d813ee9e8f1590bd106999c7
Matthew Cataldi <mcataloe@gmail.com>
2017-05-08 16:53:28 +0000

    Tried to use rpsec to detect errors, but having a hard time figuring out how to separate this into a component where there is a machine version and a user version that uses the machine version :-S.

--- ruby/game/game_class_spec.rb
+++ ruby/game/game_class_spec.rb
@@ -3,10 +3,10 @@ require 'rspec'
 # require 'debugger'
 
 describe GuessWord do
-  let(:game){ GuessWord.new }
+  let(:game){ GuessWord.new(["paper", "clipboard", "coaster", "mouse", "keyboard", "lamp", "computer", "browser"]) }
   
   it "test user interface of guessing game" do
-    expect(game_word_user(["tool", "cup"]).to eq true) || expect(game_word_user(["tool", "cup"]).to eq false)
+    expect(game.guess_word_user().to eq true) || expect(game.guess_word_user().to eq false)
   end
   
 end
