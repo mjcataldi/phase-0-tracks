e2623654242e8c9977f27821519cce6dbb16e5f8
Matthew Cataldi <mcataloe@gmail.com>
2017-05-08 19:16:28 +0000

    Figured out how to separate user-interface in class for Player 1 and Player 2

--- ruby/game/game_class.rb
+++ ruby/game/game_class.rb
@@ -3,35 +3,15 @@
 class GuessWord
   attr_accessor :word_array
   
-  def initialize(word_array)
+  def initialize(word_array = ["paper", "clipboard", "coaster", "mouse", "keyboard", "lamp", "computer", "browser"])
     @word_array = word_array
   end
   
   # Hey guys, having a hard time figuring out what kind of method to use here.  It seems that there's only sub portions that can be machine based.  Have tried my best!
 
-  def result_so_far(letter_attempts_array, word_to_guess)
-      puts ""
-      puts "Here is what you have so far: "
-      clue_word = ""
-      word_to_guess.split('').each { |letter| 
-
-        if letter_attempts_array.include?(letter)
-          clue_word += "#{letter} "
-        else
-          clue_word += "_ "
-        end
-      }
-      puts clue_word
-      puts ""
-  end
-
-  def guess_word_user()
+  def guess_word_user(word_to_guess, number_of_tries)
     number_of_tries_attempted = 0
-    number_of_tries = 8
     separator = "----------------------"
-    
-    word_to_guess = @word_array[rand(@word_array.length) + 1]
-    word_to_guess.downcase!
     
     word_attempts_array = []
     letter_attempts_array = []
@@ -105,7 +85,41 @@ class GuessWord
     end
     
   end
+  
+  def total_step_through()
+    puts "Player 1: Please enter a set of word(s) separated by a comma (,) that you wish your opponent to choose from:"
+    words_string = gets.downcase.strip
+    words_array = []
+    default_tries = 8
+    
+    if words_string.length == 0
+      words_array = @word_array
+    else
+      words_string.split(',').each { |word| words_array << word.strip }
+    end
+    word_to_guess = words_array[rand(@words_array.length + 1)]
+    puts ""
+
+
+    puts "How many tries would you like Player 2 to have (a default amount of #{default_tries} tries will be implemented if left blank)?"
+    number_of_tries = gets.strip.to_i
+    puts ""
+    
+    if number_of_tries == 0
+      number_of_tries = default_tries
+    end
+    
+    puts "Ok, Player 2 will have #{number_of_tries} to guess one of these word(s): '#{ words_string }'."
+    
+    number_of_seconds = 4
+    puts "These results will disappear in #{number_of_seconds} seconds."
+    
+    sleep number_of_seconds
+    system "clear" # or system "cls"
+    
+    guess_word_user(word_to_guess, number_of_tries)
+  end
 end
 
-new_game = GuessWord.new(["paper", "clipboard", "coaster", "mouse", "keyboard", "lamp", "computer", "browser"])
-new_game.guess_word_user()
+new_game = GuessWord.new()
+new_game.total_step_through()
