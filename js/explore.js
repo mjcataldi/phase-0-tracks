04246f1915d67ae101b6c946f95ce6301063e53d
Matthew Cataldi <mcataloe@gmail.com>
2017-05-14 22:08:14 +0000

    Commiting 7.1 Java Syntax

--- js/explore.js
+++ js/explore.js
@@ -2,33 +2,37 @@
 // drover code method first, then user interface
 
 function reverseWord (word_to_reverse){
-  word_reversed = ""
-  word_to_reverse_array = word_to_reverse.split('');
+  var word_reversed = "";
+  var word_to_reverse_array = word_to_reverse.split('');
+  
   for(var i = word_to_reverse_array.length; i > 0; i--){
-    word_reversed = word_to_reverse_array[i];
-    
+    word_reversed += word_to_reverse_array[i-1];
   }
+  
   return word_reversed;
 }
 
-function userWord()
-  var repeat = true;
 
-  while (repeat == true){
-  repeat = false;
-  console.log ("What word would you like to reverse today?");
+// // This method is not yet functional
+// function userWord(){
+//   var repeat = true;
+
+//   while (repeat == true) {
+//   repeat = false;
+//   console.log("What word would you like to reverse today?");
   
-  response = readline();
+//   // readline() is not implementable
+//   var response = readline();
   
-  if (response.length == 0){
-    console.log ("Oops! We need sone text. Let's try that again.");
-    
-    repeat = true;
-    continue;
-  }
+//   if (response.length == 0){
+//     console.log ("Oops! We need sone text. Let's try that again.");
+//     repeat = true;
+//     continue;
+//   }
   
-  console.log("Your word of " + response + "' reversed is '" + reverseWord (response);)
-  
-  }
-  
-}
+//   console.log("Your word of " + response + "' reversed is '" + reverseWord (response));
+//   }
+// }
+
+var newWord = "Flipper";
+console.log("The backwards iteration of '" + newWord + "' is '" + reverseWord(newWord).toLocaleLowerCase() + "'");
