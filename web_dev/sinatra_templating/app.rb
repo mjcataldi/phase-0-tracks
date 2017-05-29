5a646032877322f00cad8209aa4983d5fd036234
Matthew <mcataloe@gmail.com>
2017-05-29 00:19:40 +0000

    Pairing session completed for 9.5 Templating in Sinatra

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,9 +1,6 @@
 # require gems
 require 'sinatra'
-# require 'sinatra/reloader'
 require 'sqlite3'
-
-# shotgun --host 0.0.0.0 --port 8080 app.rb
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
@@ -18,11 +15,6 @@ end
 
 get '/students/new' do
   erb :new_student
-end
-
-get '/campus' do
-  @campuses = db.execute("select distinct campus from students order by campus;")
-  erb :campus
 end
 
 # create new students via
